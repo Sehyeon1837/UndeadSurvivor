a79549dfa4ca632556fd976d15259318dd326681
Seheyon <104759146+Seheyon@users.noreply.github.com>
2026-05-31 21:34:45 +0900

    방향 전환 추가

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -3,16 +3,19 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.Serialization;
 
 public class Player : MonoBehaviour
 {
     public Vector2 inputVec;
     public Rigidbody2D rigid;
+    public SpriteRenderer spriter;
     public float speed;
     
     void Awake()
     {
         rigid = GetComponent<Rigidbody2D>();
+        spriter = GetComponent<SpriteRenderer>();
     }
     
     private void FixedUpdate() 
@@ -25,5 +28,14 @@ public class Player : MonoBehaviour
     void OnMove(InputValue value)
     {
         inputVec = value.Get<Vector2>();
+    }
+
+    private void LateUpdate()
+    {
+        if (inputVec.x != 0)
+        {
+            // 0 이면 고정. -1 좌측이라 flip true, 1 우측은 flip false
+            spriter.flipX = inputVec.x < 0;
+        }
     }
 }
